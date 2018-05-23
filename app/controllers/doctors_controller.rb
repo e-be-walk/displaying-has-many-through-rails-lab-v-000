e2b6efb82a1877aba5961b2cab0a6c87f4e65461
Erin Walker <erinb0317@hotmail.com>
2018-05-23 19:36:05 +0000

    automatically backed up by learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,9 +1,10 @@
 class DoctorsController < ApplicationController
   def index
+    Doctor.find(params[:id])
   end
 
   def show
-    @doctor = doctor 
+    @doctor = doctor
     @appointments = @doctor.appointments
   end
 end
